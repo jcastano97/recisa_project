ed37eea2d3c40cc33e44c3f4d4c2dab0839e8790
jcastano1997 <jcastano1997@gmail.com>
2020-01-17 01:43:53 -0500

    Client controller

---
+++ resources/views/Client/edit.blade.php
@@ -0,0 +1,76 @@
+@extends('layouts.layout')
+@section('content')
+    <div class="row">
+        <section class="content">
+            <div class="col-md-8 col-md-offset-2">
+                @if (count($errors) > 0)
+                    <div class="alert alert-danger">
+                        <strong>Error!</strong> Revise los campos obligatorios.<br><br>
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+                @if(Session::has('success'))
+                    <div class="alert alert-info">
+                        {{Session::get('success')}}
+                    </div>
+                @endif
+
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Nuevo Cliente</h3>
+                    </div>
+                    <div class="panel-body">
+                        <div class="table-container">
+                            <form method="POST" action="{{ route('client.update',$client->id) }}"  role="form">
+                                {{ csrf_field() }}
+                                <input name="_method" type="hidden" value="PATCH">
+                                <div class="row">
+                                    <div class="col-xs-6 col-sm-6 col-md-6">
+                                        <div class="form-group">
+                                            <input type="text" name="identification_type" id="identification_type" class="form-control input-sm" placeholder="identification_type" value="{{$client->identification_type}}">
+                                        </div>
+                                    </div>
+                                    <div class="col-xs-6 col-sm-6 col-md-6">
+                                        <div class="form-group">
+                                            <input type="text" name="identification_number" id="identification_number" class="form-control input-sm" placeholder="identification_number" value="{{$client->identification_number}}">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-xs-6 col-sm-6 col-md-6">
+                                        <div class="form-group">
+                                            <input type="text" name="name" id="name" class="form-control input-sm" placeholder="name" value="{{$client->name}}">
+                                        </div>
+                                    </div>
+                                    <div class="col-xs-6 col-sm-6 col-md-6">
+                                        <div class="form-group">
+                                            <input type="text" name="phone" id="phone" class="form-control input-sm" placeholder="phone" value="{{$client->phone}}">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-xs-6 col-sm-6 col-md-6">
+                                        <div class="form-group">
+                                            <input type="text" name="email" id="email" class="form-control input-sm" placeholder="email" value="{{$client->email}}">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-xs-12 col-sm-12 col-md-12">
+                                        <input type="submit"  value="Actualizar" class="btn btn-success btn-block">
+                                        <a href="{{ route('client.index') }}" class="btn btn-info btn-block" >Atrás</a>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+    </div>
+@endsection
